f88ee526bec6660637a6b0a6f912851a418ff7be
Victor <victorma07work@gmail.com>
2022-02-01 15:25:41 -0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import MealsNavigator from "./navigation/MealsNavigator";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/Fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/Fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontLoaded, SetFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => SetFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+  return <MealsNavigator />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    paddingTop: 20,
   },
 });
