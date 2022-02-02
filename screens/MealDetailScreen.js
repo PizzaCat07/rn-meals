37e345a42cf682a62d9be0588335c0c0cc2b0694
Victor <victorma07work@gmail.com>
2022-02-02 15:16:56 -0800

    add favorite button as star

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
 import { MEALS } from "../Data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
 
-  const selectMeal = MEALS.find((meal) => meal.id === mealId);
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
     <View style={styles.screen}>
-      <Text>{selectMeal.title} </Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
-        title="Go back to Categories"
+        title="Go Back to Categories"
         onPress={() => {
           props.navigation.popToTop();
         }}
@@ -22,9 +25,20 @@ const MealDetailScreen = (props) => {
 
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam("mealId");
-  const selectMeal = MEALS.find((meal) => meal.id === mealId);
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
-    headerTitle: selectMeal.title,
+    headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite!");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
