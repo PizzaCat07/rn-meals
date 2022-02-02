6bdf316bbf93f58e0747d7c4e30910f9971a0067
Victor <victorma07work@gmail.com>
2022-02-01 16:46:31 -0800

    Setting Dynamic Navigation Options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../Data/dummy-data";
 
 const CategoryMealsScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Categories Meals Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "MealDetail" });
+        }}
+      />
     </View>
   );
 };
